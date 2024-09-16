e83ffe13d740656ac84c0dd02481bfc94feebad8
Muham <muhambriana.31@gmail.com>
2024-09-16 16:16:39 +0700

    add new activity for discount form

---
+++ app/src/main/java/com/mshell/discountcalculator/ui/DiscountFormActivity.kt
@@ -0,0 +1,26 @@
+package com.mshell.discountcalculator.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mshell.discountcalculator.R
+import com.mshell.discountcalculator.databinding.ActivityDiscountFormBinding
+
+class DiscountFormActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityDiscountFormBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
