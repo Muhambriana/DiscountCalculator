34663d19c39c92d63e6146772dc00a6e41517fff
Muham <muhambriana.31@gmail.com>
2024-10-06 20:03:12 +0700

    update DiscalResource, add throwable variable. so can print error in activity in case needed

--- app/src/main/java/com/mshell/discountcalculator/core/resource/DiscalResource.kt
+++ app/src/main/java/com/mshell/discountcalculator/core/resource/DiscalResource.kt
@@ -1,8 +1,8 @@
 package com.mshell.discountcalculator.core.resource
 
-sealed class DiscalResource<T>(val data: T? = null, val message: String? = null) {
+sealed class DiscalResource<T>(val data: T? = null, val message: String? = null, val error: Throwable? = null) {
     class Loading<T> :DiscalResource<T>()
     class Success<T>(data: T?) : DiscalResource<T>(data)
-    class Error<T>(message: String?, data: T? = null) : DiscalResource<T>(data, message)
+    class Error<T>(message: String?, error: Throwable? = null, data: T? = null) : DiscalResource<T>(data, message, error)
     class Empty<T> : DiscalResource<T>()
 }
