d9c9e4beb4f57903045243e335ee20d75ef4f87a
Muham <muhambriana.31@gmail.com>
2024-10-06 20:01:27 +0700

    new model for discount detail

---
+++ app/src/main/java/com/mshell/discountcalculator/core/models/DiscountDetail.kt
@@ -0,0 +1,20 @@
+package com.mshell.discountcalculator.core.models
+
+import android.os.Parcelable
+import com.mshell.discountcalculator.utils.config.DiscountType
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class DiscountDetail(
+
+    var discountType: DiscountType? = null,
+
+    var discountNominal: Double? = null,
+
+    var discountPercent: Int? = null,
+
+    var discountMax: Double? = null,
+
+    var additional: Double? = null
+
+): Parcelable
