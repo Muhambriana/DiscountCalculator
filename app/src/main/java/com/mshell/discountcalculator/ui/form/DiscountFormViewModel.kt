6ad95e87d2cd452d87a9e71059a96b8dd654342a
Muham <muhambriana.31@gmail.com>
2024-10-06 19:57:52 +0700

    implement viewModelScope

--- app/src/main/java/com/mshell/discountcalculator/ui/form/DiscountFormViewModel.kt
+++ app/src/main/java/com/mshell/discountcalculator/ui/form/DiscountFormViewModel.kt
@@ -2,6 +2,7 @@ package com.mshell.discountcalculator.ui.form
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mshell.discountcalculator.core.models.Form
 import com.mshell.discountcalculator.core.repository.DiscalRepository
 import com.mshell.discountcalculator.core.resource.DiscalEvent
@@ -14,11 +15,6 @@ import kotlinx.coroutines.launch
 
 class DiscountFormViewModel(private val repository: DiscalRepository): ViewModel() {
 
-    private val handler = CoroutineExceptionHandler { _, throwable ->
-        println("Caught exception: $throwable")
-    }
-    private val dispatchers = (Dispatchers.IO + handler)
-
     val resourceItemsForm = MutableLiveData<DiscalEvent<DiscalResource<List<Form>>>>()
     val resourceItemForm = MutableLiveData<DiscalEvent<DiscalResource<Form>>>()
     val discountResult = MutableLiveData<DiscalEvent<DiscalResource<List<Form>>>>()
@@ -26,7 +22,7 @@ class DiscountFormViewModel(private val repository: DiscalRepository): ViewModel
     fun getFirstList(count: Int) {
         resourceItemsForm.postValue(DiscalEvent(DiscalResource.Loading()))
 
-        CoroutineScope(dispatchers).launch {
+        viewModelScope.launch {
             val items = repository.getFirstList(count)
             async { items }.await()
             resourceItemsForm.postValue(DiscalEvent(DiscalResource.Success(items)))
@@ -36,7 +32,7 @@ class DiscountFormViewModel(private val repository: DiscalRepository): ViewModel
     fun getItem() {
         resourceItemForm.postValue(DiscalEvent(DiscalResource.Loading()))
 
-        CoroutineScope(dispatchers).launch {
+        viewModelScope.launch {
             val item = repository.getItem()
             async { item }.await()
             resourceItemForm.postValue(DiscalEvent(DiscalResource.Success(item)))
@@ -46,7 +42,7 @@ class DiscountFormViewModel(private val repository: DiscalRepository): ViewModel
     fun getResultDiscountPercent(list: MutableList<Form>?, discountPercent: Double?, discountMax: Double?) {
         discountResult.postValue(DiscalEvent(DiscalResource.Loading()))
 
-        CoroutineScope(dispatchers).launch {
+        viewModelScope.launch {
             val result = repository.getDiscountPercentResult(list, discountPercent, discountMax)
             async { result }.await()
             discountResult.postValue(DiscalEvent(DiscalResource.Success(result)))
@@ -56,7 +52,7 @@ class DiscountFormViewModel(private val repository: DiscalRepository): ViewModel
     fun getResultDiscountNominal(list: MutableList<Form>?, discountNominal: Double?) {
         discountResult.postValue(DiscalEvent(DiscalResource.Loading()))
 
-        CoroutineScope(dispatchers).launch {
+        viewModelScope.launch {
             val result = repository.getDiscountNominalResult(list, discountNominal)
             async { result }.await()
             discountResult.postValue(DiscalEvent(DiscalResource.Success(result)))
