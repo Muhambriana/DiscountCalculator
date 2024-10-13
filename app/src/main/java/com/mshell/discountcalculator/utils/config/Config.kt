3238eb83bed9432955c86c0df9ee9423e2227f88
Muham <muhambriana.31@gmail.com>
2024-10-13 09:20:17 +0700

    add new config item

--- app/src/main/java/com/mshell/discountcalculator/utils/config/Config.kt
+++ app/src/main/java/com/mshell/discountcalculator/utils/config/Config.kt
@@ -4,4 +4,7 @@ import java.util.Locale
 
 object Config {
     val LOCALE = Locale("id", "ID")
+    const val DEFAULT_DOUBLE_VALUE = 0.0
+    const val DEFAULT_DOUBLE_VALUE_ONE = 1.0
+    const val EMPTY_STRING = ""
 }
