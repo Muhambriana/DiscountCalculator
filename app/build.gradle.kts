53b1e672ab07d7a91af330f31bd3a18ee7fc8956
Muham <muhambriana.31@gmail.com>
2024-10-27 10:06:46 +0700

    increase versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mshell.discountcalculator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
