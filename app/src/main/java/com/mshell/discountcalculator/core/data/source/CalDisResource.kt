a51537855fb849854d4b689044158ebb6fc4b48e
Muham <muhambriana.31@gmail.com>
2024-11-03 22:00:52 +0700

    update data resource

--- app/src/main/java/com/mshell/discountcalculator/core/data/source/CalDisResource.kt
+++ app/src/main/java/com/mshell/discountcalculator/core/data/source/CalDisResource.kt
@@ -1,8 +1,10 @@
 package com.mshell.discountcalculator.core.data.source
 
-sealed class CalDisResource<T>(val data: T? = null, val message: String? = null, val error: Throwable? = null) {
+import com.mshell.discountcalculator.utils.config.ExceptionTypeEnum
+
+sealed class CalDisResource<T>(val data: T? = null, val message: String? = null, val error: Throwable? = null, val exceptionTypeEnum: ExceptionTypeEnum? = null) {
     class Loading<T> : CalDisResource<T>()
     class Success<T>(data: T?) : CalDisResource<T>(data)
-    class Error<T>(message: String?, error: Throwable? = null, data: T? = null) : CalDisResource<T>(data, message, error)
+    class Error<T>(message: String?, error: Throwable? = null, exceptionTypeEnum: ExceptionTypeEnum? = null,data: T? = null) : CalDisResource<T>(data, message, error, exceptionTypeEnum)
     class Empty<T> : CalDisResource<T>()
 }
