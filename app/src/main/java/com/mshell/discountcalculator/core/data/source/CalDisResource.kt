9172bffb8e9911447ec234e997d35403195ecdc2
Muham <muhambriana.31@gmail.com>
2024-11-10 11:16:51 +0700

    define default value

--- app/src/main/java/com/mshell/discountcalculator/core/data/source/CalDisResource.kt
+++ app/src/main/java/com/mshell/discountcalculator/core/data/source/CalDisResource.kt
@@ -5,6 +5,6 @@ import com.mshell.discountcalculator.utils.config.ExceptionTypeEnum
 sealed class CalDisResource<T>(val data: T? = null, val message: String? = null, val error: Throwable? = null, val exceptionTypeEnum: ExceptionTypeEnum? = null) {
     class Loading<T> : CalDisResource<T>()
     class Success<T>(data: T?) : CalDisResource<T>(data)
-    class Error<T>(message: String?, error: Throwable? = null, exceptionTypeEnum: ExceptionTypeEnum? = null,data: T? = null) : CalDisResource<T>(data, message, error, exceptionTypeEnum)
+    class Error<T>(message: String? = null, error: Throwable? = null, exceptionTypeEnum: ExceptionTypeEnum? = null,data: T? = null) : CalDisResource<T>(data, message, error, exceptionTypeEnum)
     class Empty<T> : CalDisResource<T>()
 }
