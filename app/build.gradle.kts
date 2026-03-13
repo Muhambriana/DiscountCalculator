3c0ebd81ca67e7904e661438c8c990db9f71052d
muham <muhambriana.31@gmail.com>
2026-03-13 11:22:36 +0700

    update compileSdk and targetSdk to 36, and versionName to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.mshell.discountcalculator"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mshell.discountcalculator"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
