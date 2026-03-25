85ea9b91eeac3cffc6b94ea24ac6204048f7de07
muham <muhambriana.31@gmail.com>
2026-03-25 08:17:59 +0700

    fix: update versionCode to 1 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.mshell.discalc"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 1
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
